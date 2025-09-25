e8250f4d65439b0da6734740d83e329406754a69
tobiashardoe <tobias.hardoe@gmail.com>
2025-09-25 12:54:03 +0200

    Task 2

--- TorsdagsOpgave 4/Task 2/Main.java
+++ TorsdagsOpgave 4/Task 2/Main.java
@@ -1,11 +1,36 @@
 import java.util.Scanner;
 
+// task 2.a
 class Main{
 
-    // task 2.a
-	public void main(String [] args){
+    
+	public static void main(String [] args){
+
+        // task 2.b
+		System.out.println("Please type your name:");
+
+		// task 2.c
+		Scanner sc = new Scanner(System.in);
+
+		// task 2.d
+		String name = sc.nextLine();
+
+		// task 2.e
+		System.out.println("Hello " + name +", " + "\nPlease type your age:");
+
+        // task 2.f
+		int age = sc.nextInt();
+
+		// task 2.g
+		System.out.println("You are " + age + " years old." + "\nPlease type the age your retirement starts:");
+
+		// task 2.h
+		int retirement = sc.nextInt();
+        System.out.println("You have " + (retirement - age) + " years until retirement.");
 
 
 
 	}
+
+
 }
